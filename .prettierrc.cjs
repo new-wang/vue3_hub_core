274e8ce25f3fbeab888a2a5c0aeeb03ec8d7fe04
new-wang <1569566735@qq.com>
2022-08-04 14:47:06 +0800

    eslint prettier配置

---
+++ .prettierrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+    // printWidth: 80,
+    // tabWidth: 2,
+    // useTabs: false,
+    semi: false, // 句末是否使用分号，default: true
+    singleQuote: true, // 单引号 default: false
+    // quoteProps: 'as-needed',
+    // jsSingleQuote: false,
+    trailingComma: "none", // 多行时尽可能打印尾随逗号 default: es5     all
+    // bracketSpacing: true,
+    // bracketSameLine: false,
+    // jsxBracketSameLine: false,
+    arrowParents: "avoid", // default: always
+    // insertPragma: false,
+    // requirePragma: false,
+    proseWrap: "never",
+    // htmlWhitespaceSensitivity: 'CSS',
+    // vueIndentScriptAndStyle: false, // .vue 缩进
+    endOfLine: "auto", // default lf
+}
